9d5c54ad5876012fb0de1419f15392a6bc4c2758
MUM <otaku.ict@gmail.com>
2022-03-01 17:58:35 +0700

    echo implement

--- handler/customer.go
+++ handler/customer.go
@@ -1,13 +1,14 @@
 package handler
 
 import (
-	"encoding/json"
+	//"encoding/json"
 	"fmt"
 	"hexa/service"
 	"net/http"
 	"strconv"
 
-	"github.com/gorilla/mux"
+	//"github.com/gorilla/mux"
+	"github.com/labstack/echo/v4"
 )
 
 type customerHandler struct {
@@ -18,29 +19,54 @@ func NewCustomerHandler(custSrv service.CustomerService) customerHandler {
 	return customerHandler{custSrv: custSrv}
 }
 
-func (h customerHandler) GetCustomers(w http.ResponseWriter, r *http.Request) {
+// func (h customerHandler) GetCustomers(w http.ResponseWriter, r *http.Request) {
+// 	customers, err := h.custSrv.GetCustomers()
+// 	if err != nil {
+// 		w.WriteHeader(http.StatusInternalServerError)
+// 		fmt.Fprintln(w, err)
+// 		return
+// 	}
+
+// 	w.Header().Set("content-type", "application/json")
+// 	json.NewEncoder(w).Encode(customers)
+// }
+
+func (h customerHandler) GetCustomers(c echo.Context) error {
 	customers, err := h.custSrv.GetCustomers()
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintln(w, err)
-		return
-	}
 
-	w.Header().Set("content-type", "application/json")
-	json.NewEncoder(w).Encode(customers)
+		fmt.Fprintln(nil, err)
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"err": "invalid data type",
+		})
+	}
+	return c.JSON(http.StatusOK, customers)
 }
 
-func (h customerHandler) GetCustomer(w http.ResponseWriter, r *http.Request) {
-	customerID, _ := strconv.Atoi(mux.Vars(r)["customerID"]) //map request ด้วยkey customerIDโดยใช้mux  แล้วแปลงเป็นintegerเพราะ customer ด้านล่างต้องการint
+// func (h customerHandler) GetCustomer(w http.ResponseWriter, r *http.Request) {
+// 	customerID, _ := strconv.Atoi(mux.Vars(r)["customerID"]) //map request ด้วยkey customerIDโดยใช้mux  แล้วแปลงเป็นintegerเพราะ customer ด้านล่างต้องการint
+
+// 	customer, err := h.custSrv.GetCustomer(customerID)
+// 	if err != nil {
+// 		w.WriteHeader(http.StatusInternalServerError)
+// 		fmt.Fprintln(w, err)
+
+// 		return
+// 	}
+
+// 	w.Header().Set("content-type", "application/json")
+// 	json.NewEncoder(w).Encode(customer)
+// }
+
+func (h customerHandler) GetCustomer(c echo.Context) error {
+	customerID, _ := strconv.Atoi(c.Param("id")) //map request ด้วยkey customerIDโดยใช้mux  แล้วแปลงเป็นintegerเพราะ customer ด้านล่างต้องการint
 
 	customer, err := h.custSrv.GetCustomer(customerID)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintln(w, err)
 
-		return
+		fmt.Fprintln(nil, err)
+		return err
 	}
 
-	w.Header().Set("content-type", "application/json")
-	json.NewEncoder(w).Encode(customer)
+	return c.JSON(http.StatusOK, customer)
 }
--- main.go
+++ main.go
@@ -4,14 +4,17 @@ import (
 	"hexa/handler"
 	"hexa/repository"
 	"hexa/service"
-	"net/http"
+
+	//"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
+	//"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	e := echo.New()
 	db, err := sqlx.Open("mysql", "root@tcp(127.0.0.1:3306)/banking")
 	if err != nil {
 		panic(err)
@@ -23,11 +26,17 @@ func main() {
 
 	customerService := service.NewCustomerService(customerRepositoryMock)
 	customerHandler := handler.NewCustomerHandler(customerService)
+	//Router
 
-	router := mux.NewRouter()
+	e.GET("/customers", customerHandler.GetCustomers)
+	e.GET("/customers/:id", customerHandler.GetCustomer)
+	//router := mux.NewRouter()
 
-	router.HandleFunc("/customers", customerHandler.GetCustomers).Methods(http.MethodGet)
-	router.HandleFunc("/customers/{customerID:[0-9]+}", customerHandler.GetCustomer).Methods(http.MethodGet)
+	//router.HandleFunc("/customers", customerHandler.GetCustomers).Methods(http.MethodGet)
+	//router.HandleFunc("/customers/{customerID:[0-9]+}", customerHandler.GetCustomer).Methods(http.MethodGet)
 
-	http.ListenAndServe(":8000", router)
+	//http.ListenAndServe(":8000", router)
+	e.Logger.Fatal(e.Start(":1323"))
 }
+
+// e.GET("/users/:id", getUser)
